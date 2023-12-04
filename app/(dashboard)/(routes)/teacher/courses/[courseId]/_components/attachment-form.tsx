f832dc2f253f972e020a9b1d0d648fcff88689a8
hant <captainnemot1k60@gmail.com>
2023-12-05 01:30:40 +0700

    complete attachment form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/attachment-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/attachment-form.tsx
@@ -3,7 +3,7 @@
 import * as z from "zod";
 import axios from "axios";
 import { Button } from "@/components/ui/button";
-import { ImageIcon, Pencil, PlusCircle } from "lucide-react";
+import { File, ImageIcon, Loader2, Pencil, PlusCircle, X } from "lucide-react";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
@@ -27,13 +27,16 @@ export const AttachmentForm = (
         initialData,
         courseId
     }: AttachmentFormProps) => {
+
     const [isEditing, setIsEditing] = useState(false);
+    const [deletingId, setDeletingId] = useState<string | null>(null);
 
     const toggleEdit = () => {
         setIsEditing((current) => !current);
     }
     
     const router = useRouter();
+
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
         try {
             await axios.post(`/api/courses/${courseId}/attachments`, values);
@@ -44,6 +47,19 @@ export const AttachmentForm = (
             toast.error("Something went wrong");
         }
         // console.log(values);
+    }
+
+    const onDelete = async (id: string) => {
+        try {
+            setDeletingId(id);
+            await axios.delete(`/api/courses/${courseId}/attachments/${id}`);
+            toast.success("Attachment deleted");
+            router.refresh();
+        } catch {
+            toast.error("Something went wrong");
+        } finally {
+            setDeletingId(null);
+        }
     }
 
     return (
@@ -68,6 +84,34 @@ export const AttachmentForm = (
                         <p className="text-sm mt-2 text-slate-500 italic">
                             No attachments yet
                         </p>
+                    )}
+                    {initialData.attachments.length > 0 && (
+                        <div className="space-y-2">
+                            {initialData.attachments.map((attachment) => (
+                                <div 
+                                key = {attachment.id}
+                                    className="flex items-center p-3 w-full bg-sky-100 border-sky-200 border text-sky-700 rounded-md"
+                                >
+                                <File className="h-4 w-4 mr-2 flex-shrink-0"/>
+                                <p className="text-xs line-clamp-1">
+                                    {attachment.name}
+                                </p>
+                                {deletingId === attachment.id && (
+                                    <div className="ml-auto">
+                                        <Loader2 className="h-4 w-4 animate-spin"/>
+                                    </div>
+                                )}
+                                {deletingId !== attachment.id && (
+                                    <button
+                                        onClick={() => onDelete(attachment.id)}
+                                        className="ml-auto hover:opacity-75 transition"
+                                    >
+                                        <X className="h-4 w-4 "/>
+                                    </button>
+                                )}
+                                </div>
+                            ))}
+                        </div>
                     )}
                 </>
             )}
