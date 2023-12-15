680e89b5beb8952eb62eb6c57e071ce5581007da
hant <captainnemot1k60@gmail.com>
2023-12-16 06:53:47 +0700

    ok

--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -1,6 +1,12 @@
+import Mux from "@mux/mux-node";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import {db} from "@/lib/db"
+
+const { Video } = new Mux(
+    process.env.MUX_TOKEN_ID!,
+    process.env.MUX_TOKEN_SECRET!,
+);
 
 export async function PATCH(
     req: Request,
@@ -30,6 +36,37 @@ export async function PATCH(
                 ...values,
             }
         });
+
+        if(values.videoUrl) {
+            const existingMuxData = await db.muxData.findFirst({
+                where: {
+                    chapterId: params.chapterId
+                }
+            });
+
+            if(existingMuxData) {
+                await Video.Assets.del(existingMuxData.assetId);
+                await db.muxData.delete({
+                    where: {
+                        id: existingMuxData.id
+                    }
+                });
+            }
+
+            const asset = await Video.Assets.create({
+                input: values.videoUrl,
+                playback_policy: "public",
+                test: false
+            });
+
+            await db.muxData.create({
+                data: {
+                    assetId: asset.id,
+                    chapterId: params.chapterId,
+                    playbackId: asset.playback_ids?.[0].id,
+                }
+            });
+        }
 
         return NextResponse.json(chapter);
     } catch (error) {
